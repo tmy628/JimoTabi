72c75ce53c38a6323fc19c73b4522943495b6631
tmy628 <tmy6h1r@gmail.com>
2021-06-07 07:55:47 +0000

    [add] ゲストログイン機能を実装 #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  #ゲストユーザー用のアカウント作成
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.alphanumeric(6) 
+      user.name = "ゲスト"
+    end
+  end
 end
