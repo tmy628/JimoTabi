d4d77e5d296906825b9a1e168fba773f41c4efa7
tmy628 <tmy6h1r@gmail.com>
2021-06-07 11:33:37 +0000

    [add] TagMapモデル・tag_mapsテーブルを作成 #5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_112403) do
+ActiveRecord::Schema.define(version: 2021_06_07_112852) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_06_07_112403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tag_maps", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_tag_maps_on_post_id"
+    t.index ["tag_id"], name: "index_tag_maps_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
