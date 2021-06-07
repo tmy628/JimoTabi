1520527f9c8eefcabcc899f4525a679994e25e7a
tmy628 <tmy6h1r@gmail.com>
2021-06-07 11:47:08 +0000

    [add] Likeモデル・likesテーブルを作成 #5

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ApplicationRecord
 
   has_many :tag_maps, dependent: :destroy
   has_many :tags, through: :tag_maps
+  has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   #ゲストユーザー用のアカウント作成
   def self.guest
