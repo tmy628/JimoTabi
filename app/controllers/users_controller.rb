b0c3424332f9286507047de9ffb7300be63fc1b2
tmy628 <tmy6h1r@gmail.com>
2021-06-08 14:40:21 +0000

    [add] ユーザ詳細ページを作成 #36

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).reverse_order
+    # ユーザに関連づけられた投稿のみ、@postsに渡す
   end
 
   def edit
