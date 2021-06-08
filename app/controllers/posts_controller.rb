2cbed9f3a368b48becf2b38a3e2ee8a66c21e265
tmy628 <tmy6h1r@gmail.com>
2021-06-08 05:46:09 +0000

    [add] 投稿一覧画面を作成 #12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,12 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to post_path(@post)
+    redirect_to posts_path(@post)
   end
 
   def index
+    @posts = Post.all.order(created_at: :desc)
+    # 新しい投稿が上にくるように設定
   end
 
   def show
