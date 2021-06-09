81f406d6aebebe909638ee73119cad18c3888f51
tmy628 <tmy6h1r@gmail.com>
2021-06-09 01:00:26 +0000

    [add] プロフィール編集ページを作成・編集ページへのリンクを設定 #37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).reverse_order
-    # ユーザに関連づけられた投稿のみ、@postsに渡す
+    # ユーザーに関連づけられた投稿のみ、@postsに渡す
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
