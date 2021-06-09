ed900aaaabe6749df4377c37617b02a2b0a15ac2
tmy628 <tmy6h1r@gmail.com>
2021-06-09 01:04:48 +0000

    [add] 編集データの保存機能を作成 #37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
