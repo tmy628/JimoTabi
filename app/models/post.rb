a9687385480ef8fcdc405815b94c0287bb779ee9
tmy628 <tmy6h1r@gmail.com>
2021-06-16 00:49:25 +0000

    [add] Postモデルに、save_tagインスタンスメソッドを定義 #50

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,4 +17,22 @@ class Post < ApplicationRecord
   def liked_by(user)
     Like.find_by(user_id: user.id, post_id: id)
   end
+
+  def save_tag(sent_tags)
+    # 現在存在するタグ・古いタグ・新しいタグをそれぞれ取得
+    current_tags = salf.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    # 古いタグの削除
+    old_tags.each do |old|
+      self.post_tags.delete PostTag.find_by(tag_name: old)
+    end
+
+    # 新しいタグの保存
+    new_tags.each do |new|
+      new_post_tag = PostTag.find_or_create_by(tag_name: new)
+      self.post_tags << new_post_tag
+    end
+  end
 end
