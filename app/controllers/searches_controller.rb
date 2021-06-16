a20c3743843e090a7f7dc491e59695f90c49c9b8
tmy628 <tmy6h1r@gmail.com>
2021-06-16 14:08:31 +0000

    [add] searchesコントローラにindexアクションを作成する #51

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,7 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @posts = Post.search(params[:search]).limit(132)
+    @search = params[:search]
   end
 end
