cf904fc3ddad47db795432a95a66d7805530d7e7
tmy628 <tmy6h1r@gmail.com>
2021-06-21 07:43:18 +0000

    [add] 投稿フォームに必須項目を追記 #85
    [fix] spotテーブルのカラムにかけたNN制約を解除 #80

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_114224) do
+ActiveRecord::Schema.define(version: 2021_06_21_070352) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 2021_06_07_114224) do
   end
 
   create_table "spots", force: :cascade do |t|
-    t.string "address", null: false
-    t.float "latitude", null: false
-    t.float "longitude", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
