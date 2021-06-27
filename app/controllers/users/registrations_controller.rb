aa83081c3588d6ce1e8aa58292795f3d27282057
tmy628 <tmy6h1r@gmail.com>
2021-06-27 01:54:56 +0000

    [fix] ゲストユーザーがメールアドレス・パスワードを変更できないように設定 #135

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: :destroy
+  before_action :ensure_normal_user, only: %i[update destroy]
 
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       redirect_to root_path
     end
   end
-
 end
