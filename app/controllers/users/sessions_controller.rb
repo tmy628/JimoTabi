5cf715126ae2b6aac3b630bdeeb167b0fdfe0278
tmy628 <tmy6h1r@gmail.com>
2021-06-30 13:09:04 +0000

    [fix] ゲストログインをした際の遷移先を変更 #159

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,6 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to posts_path
+    redirect_to user_path(current_user.id)
   end
 end
