a8108f3bb20f2a23f26cce244ab45e7521953b0e
tmy628 <tmy6h1r@gmail.com>
2021-07-06 04:49:20 +0000

    [add] commentsコントローラを編集 #168

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    # 投稿に紐づいたコメントを作成
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    render :index
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comments.destroy
+    render :index
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
 end
