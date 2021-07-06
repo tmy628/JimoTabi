ba36d522358d1da31f3a859d8ad95a664239669f
tmy628 <tmy6h1r@gmail.com>
2021-07-06 13:32:50 +0000

    [fix] commentsコントローラを修正 #168

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @post = Post.find(params[:post_id])
     # 投稿に紐づいたコメントを作成
-    @comment = @post.comments.build(comment_params)
+    @comment = Comment.new(comment_params)
+    @comment.post_id = @post.id
     @comment.user_id = current_user.id
     @comment.save
-    render :index
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @comments.destroy
-    render :index
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+    comment.destroy
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content)
   end
 end
