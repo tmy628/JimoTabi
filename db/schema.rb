ccc39aadc29e5f0fb026f0d3be5d5e5cf5f294e8
tmy628 <tmy6h1r@gmail.com>
2021-07-10 08:46:49 +0000

    [add] Postsテーブルのprefecture_codeカラムにNULL制約を設定 #160

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_024701) do
+ActiveRecord::Schema.define(version: 2021_07_06_014943) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -32,11 +32,11 @@ ActiveRecord::Schema.define(version: 2021_07_10_024701) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "caption", null: false
+    t.integer "prefecture_code", null: false
     t.string "image_id"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "prefecture_code"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
