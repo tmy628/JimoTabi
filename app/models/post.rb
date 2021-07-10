e55af8815f621f3336ef89080d5914a03fd17010
tmy628 <tmy6h1r@gmail.com>
2021-07-10 06:40:50 +0000

    [fix] Postモデルを編集 #160

--- app/models/post.rb
+++ app/models/post.rb
@@ -47,4 +47,17 @@ class Post < ApplicationRecord
     return Post.all unless search
     Post.where(['title LIKE ? OR caption LIKE ? ', "%#{search}%", "%#{search}%"])
   end
+
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  # 都道府県コードから都道府県名に自動で変換する
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+  # ~.prefecture_nameで都道府県名を参照可能にする。
 end
